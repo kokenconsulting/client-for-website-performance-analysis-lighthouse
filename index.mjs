57dc8eb50ae11286211334d1c68eb8e3dae93f34
Ali Koeken <ali.koeken.osv@fedex.com>
2023-05-26 18:27:25 +0200

    push trial

--- index.mjs
+++ index.mjs
@@ -11,7 +11,8 @@ import {
 import * as path from 'path';
 import * as fs from 'fs';
 import { v4 as uuidv4 } from 'uuid';
-
+import * as exec from 'child_process';
+const { TEST_LOGIN_NAME, TEST_LOGIN_PW, GITHUB_PAGES, DEBUG = false } = process.env;
 function GetAppInfoInstance() {
     const githubRepoUrl = 'https://github.com/repo';
     const githubBranchName = 'main';
@@ -43,4 +44,45 @@ await createSummaryForSession(appInfo, reportsFolderAbsolutePath, sessionId);
 //wait until compile data for session is done
 prepareThrottlingChartDataForSession(appInfo, sessionId, reportsFolderAbsolutePath);
 prepareSessionListForApp(appInfo, reportsFolderAbsolutePath);
+// await pushChanges();
+// async function pushChanges(){
+//     try {
+//         await terminal(`git config --global user.email 'ci.automation@example.com'`);
+//         await terminal(`git config --global user.name 'CI Automation'`);
+//         await terminal(`git add --all`);
+//         await terminal(`git commit -m 'Lighthouse tests'`);
+//         await terminal(
+//             `git push https://${GITHUB_PAGES}@github.com/FedEx/eai-3536169-frontend.git HEAD:debug-lighthouse -f`,
+//         );
+//     } catch (err) {
+//         console.error(err);
+//         process.exit(1);
+//     }
+// }
 
+// const terminal = (command) =>
+//     new Promise((resolve, reject) => {
+//         const commandLine = exec(command);
+//         let output = '';
+//         let error = '';
+
+//         commandLine.stdout.on('data', (data) => {
+//             const outputString = data.toString();
+//             output += outputString;
+//             console.log(outputString);
+//         });
+
+//         commandLine.stderr.on('data', (data) => {
+//             const errorString = data.toString();
+//             error += errorString;
+//             console.error(errorString);
+//         });
+
+//         commandLine.on('exit', (code) => {
+//             resolve({
+//                 output,
+//                 error,
+//                 code: code.toString(),
+//             });
+//         });
+//     });
