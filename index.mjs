0299d34f5aed310820f385a4984007127cdae50a
Ali Koeken <ali.koeken.osv@fedex.com>
2023-05-26 17:34:11 +0200

    test github actions

---
+++ index.mjs
@@ -0,0 +1,47 @@
+//import { runAnalysisWithBuiltInThrottling, runAnalysisWithExternalThrottling,AppInfo, logInfo,prepareThrottlingChartDataForSession,compileDataForSession, } from '/opt/homebrew/lib/node_modules/website-performance-analysis-lighthouse-draft/index.js';
+import {
+    runAnalysisWithBuiltInThrottling,
+    runAnalysisWithExternalThrottling,
+    AppInfo,
+    logInfo,
+    prepareThrottlingChartDataForSession,
+    createSummaryForSession,
+    prepareSessionListForApp
+} from 'website-performance-analysis-lighthouse-draft';
+import * as path from 'path';
+import * as fs from 'fs';
+import pkg from '/opt/homebrew/lib/node_modules/uuid/dist/index.js';
+const { v4: uuidv4 } = pkg;
+
+function GetAppInfoInstance() {
+    const githubRepoUrl = 'https://github.com/repo';
+    const githubBranchName = 'main';
+    const appName = 'AppName';
+    const appVersion = '0.0.1';
+    const appInfo = new AppInfo(appName, appVersion, githubRepoUrl, githubBranchName, "performance folder");
+    return appInfo;
+}
+const url = 'https://www.google.com'
+const appInfo = GetAppInfoInstance();
+
+//get current directory of the mjs file
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+const localReportsFolderPath = __dirname + '/reports'
+if (!fs.existsSync(localReportsFolderPath)) {
+    fs.mkdirSync(localReportsFolderPath);
+    logInfo(`Reports folder created at ${localReportsFolderPath}`);
+}
+
+const sessionId = uuidv4();
+logInfo(`session id is ${sessionId}`);
+
+var reportsFolderAbsolutePath = path.resolve(localReportsFolderPath);
+logInfo(`Saving reports to ${reportsFolderAbsolutePath}`)
+
+await runAnalysisWithExternalThrottling(appInfo, url, reportsFolderAbsolutePath, sessionId);
+await createSummaryForSession(appInfo, reportsFolderAbsolutePath, sessionId);
+//wait until compile data for session is done
+prepareThrottlingChartDataForSession(appInfo, sessionId, reportsFolderAbsolutePath);
+prepareSessionListForApp(appInfo, reportsFolderAbsolutePath);
+
