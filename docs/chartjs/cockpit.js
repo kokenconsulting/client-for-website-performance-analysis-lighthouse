1d33af5688da7b972e42665bd15d5ab0b2a78637
Ali Koeken <ali.koeken.osv@fedex.com>
2023-06-01 18:43:32 +0200

    updates

--- docs/chartjs/cockpit.js
+++ docs/chartjs/cockpit.js
@@ -1,4 +1,21 @@
 var mainChartObject;
+var throttledAuditGroupChartObject;
+var constants = {
+    auditlistSuffix: "_auditlist.json",
+    ThrottleImpactReportFileName: "web-page-throttled-audit-throttle-impact-report.json",
+    reportsFolderName: "reports",
+    AUDITS_NAME: "audits",
+    CHART_DATA_FOLDER_NAME: "chartdata",
+    ALL_KEYS: "allkeys.json",
+    AUDIT_INSTANCE_ID: "throttledAuditGroupId"
+};
+
+var baseUrl = "localhost:8000";
+var applicationId = "Google_Support_Page";
+var colorList = ["red", "blue", "green", "orange", "purple", "yellow", "pink", "brown", "grey", "black"];
+var savedCpuSlowDownMultipliers = [];
+var savedResultTypes = [];
+var firstRun = true;
 class DATA_SOURCES {
     static REPORTS_FOLDER_NAME() {
         return "reports";
