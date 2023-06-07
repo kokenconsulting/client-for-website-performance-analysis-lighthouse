824fb41adad9887a66d6f18fe3bc8c3b00823c33
Ali Koeken <ali.koeken.osv@fedex.com>
2023-06-07 22:34:01 +0200

    new version

--- src/runner.ts
+++ src/runner.ts
@@ -1,51 +1,48 @@
 import * as path from 'path';
 import {
-  PerformanceMonitorOrchestrator,
-}
-  from 'website-performance-analysis-lighthouse-draft';
+  PerformanceMonitorOrchestrator
+} from 'website-performance-analysis-lighthouse-draft';
 
-async function TestConfig() {
+async function TestConfig(): Promise<void> {
   //get current working directory
   const configFullPath = path.join(process.cwd(), 'auditConfigurations/test-config.json');
   const auditRunner = new PerformanceMonitorOrchestrator(configFullPath);
   await auditRunner.run(false);
 }
 
-async function GoogleSupportLP() {
+async function GoogleSupportLP(): Promise<void> {
   //get current working directory
   const configFullPath = path.join(process.cwd(), 'auditConfigurations/Google-Support-LP.json');
   const auditRunner = new PerformanceMonitorOrchestrator(configFullPath);
   await auditRunner.run(true);
 }
-async function MagicRatingLandingL6() {
+
+async function MagicRatingLandingL6(): Promise<void> {
   //get current working directory
   const configFullPath = path.join(process.cwd(), 'auditConfigurations/MagicRating-Landing-L6.json');
   const auditRunner = new PerformanceMonitorOrchestrator(configFullPath);
   await auditRunner.run(true);
 }
-async function MagicRatingLandingLP() {
+
+async function MagicRatingLandingLP(): Promise<void> {
   //get current working directory
   const configFullPath = path.join(process.cwd(), 'auditConfigurations/MagicRating-Landing-LP.json');
   const auditRunner = new PerformanceMonitorOrchestrator(configFullPath);
   await auditRunner.run(true);
 }
-async function DCC_CS_LandingLP() {
+
+async function DCC_CS_LandingLP(): Promise<void> {
   //get current working directory
   const configFullPath = path.join(process.cwd(), 'auditConfigurations/DCC-CS-Landing-LP.json');
   const auditRunner = new PerformanceMonitorOrchestrator(configFullPath);
   await auditRunner.run(true);
 }
-async function DCC_CS_ProfileLP() {
-  //get current working directory
-  const configFullPath = path.join(process.cwd(), 'auditConfigurations/DCC-CS-Profile-LP.json');
-  const auditRunner = new PerformanceMonitorOrchestrator(configFullPath);
-  await auditRunner.run(true);
+
+async function runAudits(): Promise<void> {
+  await DCC_CS_LandingLP();
+  await MagicRatingLandingL6();  
+  await MagicRatingLandingLP();
+
 }
 
-//await TestConfig();
-
-//await GoogleSupportLP();
-await DCC_CS_LandingLP();
-await DCC_CS_ProfileLP();
-await MagicRatingLandingL6();
-await MagicRatingLandingLP();
+runAudits();
